8cf493150fd5f3983fcd97d5eaf549beda2ca9b4
Roxanne Weber <marie.sankofa@gmail.com>
2021-10-31 23:22:55 -0500

    destructured variables and mapped new array of objects

--- src/index.js
+++ src/index.js
@@ -14,50 +14,41 @@ import './index.css';
 // className instead of class
 // formatting
 
-//setup variables
-const firstBook = {
-	img: 'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg',
-	title: 'I Love You to the Moon and Back',
-	author: 'Amelia Hayworth',
-};
+//setup objects array
+const books = [
+	{
+		img: 'https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg',
+		title: 'I Love You to the Moon and Back',
+		author: 'Amelia Hayworth',
+	},
 
-const secondBook = {
-	img: 'https://m.media-amazon.com/images/I/51iP-k3nWeL.jpg',
-	title: "The Judge's List",
-	author: 'John Grisham',
-};
+	{
+		img: 'https://m.media-amazon.com/images/I/51iP-k3nWeL.jpg',
+		title: "The Judge's List",
+		author: 'John Grisham',
+	},
+];
 
 function BookList() {
 	return (
 		<section className='booklist'>
-			<Book
-				img={firstBook.img}
-				title={firstBook.title}
-				author={firstBook.author}
-			>
-				<p>
-					{' '}
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
-					exercitationem. Numquam, debitis nostrum cum delectus sapiente.
-				</p>
-			</Book>
-			<Book
-				img={secondBook.img}
-				title={secondBook.title}
-				author={secondBook.author}
-			/>
+			{books.map((book) => {
+				const { img, title, author } = book;
+				return <Book book={book}></Book>;
+			})}
+			;
 		</section>
 	);
 }
 
-const Book = ({ img, title, author, children }) => {
-	// const { img, title, author } = props;
+const Book = (props) => {
+	const { img, title, author } = props.book;
 	return (
 		<article className='book'>
 			<img src={img} alt='' />
 			<h1>{title}</h1>
 			<h3>{author}</h3>
-			{children}
+			{/* {children} */}
 
 			{/* {console.log(props)} */}
 		</article>
