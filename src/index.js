2d42013e10ca5c23ecdb7a934ea4b0de0c625de8
Roxanne Weber <marie.sankofa@gmail.com>
2021-10-31 21:16:21 -0500

    completion of mini book project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// CSS
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// stateless functional component
+// always return JSX
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//! JSX Rules
+// return single element
+// div / section / article or Fragment
+// use camelCase for html attributes
+// className instead of class
+// formatting
+
+function BookList() {
+	return (
+		<section className='booklist'>
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+			<Book />
+		</section>
+	);
+}
+
+const Book = () => {
+	return (
+		<article className='book'>
+			<Image />
+			<Title />
+			<Author />
+		</article>
+	);
+};
+
+const Image = () => {
+	return (
+		<img
+			src='https://images-na.ssl-images-amazon.com/images/I/517h-u1AQlL._SX482_BO1,204,203,200_.jpg'
+			alt=''
+		/>
+	);
+};
+
+const Title = () => {
+	return <h1>I Love You To the Moon and Back</h1>;
+};
+
+const Author = () => {
+	return <h3>Amelia Hayworth</h3>;
+};
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
